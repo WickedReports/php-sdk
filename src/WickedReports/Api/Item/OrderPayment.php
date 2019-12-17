1d1a1f05953e23c26199850f8e63e7bff3264807
moskeet-wickedreports <moskeet@wickedreports.com>
2019-12-17 11:08:21 +0200

    bugfix/statuses: statuses limitation removed, statuses used in sql were added to library

--- src/WickedReports/Api/Item/OrderPayment.php
+++ src/WickedReports/Api/Item/OrderPayment.php
@@ -8,15 +8,11 @@ class OrderPayment extends BaseItem
 {
     const STATUS_DEFAULT = '';
     const STATUS_APPROVED = 'APPROVED';
-    const STATUS_FAILED = 'FAILED';
+    const STATUS_VOIDED = 'VOIDED';
     const STATUS_REFUNDED = 'REFUNDED';
     const STATUS_PARTIALLY_REFUNDED = 'PARTIALLY REFUNDED';
-    const STATUS_VOIDED = 'VOIDED';
+    const STATUS_FAILED = 'FAILED';
     const STATUS_DECLINED = 'DECLINED';
-    const STATUS_CHARGEBACK = 'CHARGEBACK';
-    const STATUS_CHARGEBACKS = 'CHARGEBACKS';
-    const STATUS_ERROR = 'ERROR';
-    const STATUS_WRITTEN = 'WRITTEN';
 
     /**
      * @var array
@@ -33,21 +29,7 @@ class OrderPayment extends BaseItem
             ->key('OrderID', v::stringType()->notEmpty()->length(0, 500))
             ->key('PaymentDate', v::date('Y-m-d H:i:s'))
             ->key('Amount', v::numeric()->min(0))
-            ->key('Status', v::stringType()->length(0, 500)->in(
-                [
-                    self::STATUS_DEFAULT,
-                    self::STATUS_APPROVED,
-                    self::STATUS_FAILED,
-                    self::STATUS_REFUNDED,
-                    self::STATUS_PARTIALLY_REFUNDED,
-                    self::STATUS_VOIDED,
-                    self::STATUS_DECLINED,
-                    self::STATUS_CHARGEBACK,
-                    self::STATUS_CHARGEBACKS,
-                    self::STATUS_ERROR,
-                    self::STATUS_WRITTEN,
-                ])
-            )
+            ->key('Status', v::stringType()->length(0, 500))
         ;
     }
 }
