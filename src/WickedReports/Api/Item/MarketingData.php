89ea53ba8174992c603a973750b2be7dc4e184fc
Aleksandr Kuzenkov <alexandr.kuzenkov@wickedreports.com>
2022-03-21 14:26:34 -0500

    WR-2665 Marketing API added params

--- src/WickedReports/Api/Item/MarketingData.php
+++ src/WickedReports/Api/Item/MarketingData.php
@@ -22,6 +22,10 @@ class MarketingData extends BaseItem
             ->key('Cost', v::numeric()->notEmpty()->length(0, 18))
             ->key('AdId', v::stringType()->notEmpty()->length(0, 500))
             ->key('AccountId', v::stringType()->notEmpty()->length(0, 500))
+            ->key('CampaignId', v::numeric()->notEmpty()->length(0, 18))
+            ->key('AdGroupId', v::stringType()->notEmpty()->length(0, 500))
+            ->key('ConversionCount', v::numeric()->notEmpty()->length(0, 18))
+            ->key('ConversionValue', v::numeric()->notEmpty()->length(0, 18))
         ;
     }
 }
